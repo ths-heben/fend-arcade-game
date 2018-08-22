d7159ba5c879de88420e49661724775cb5eb5470
Thomas Hebenstreit <hebenstreit.th@gmail.com>
2018-08-22 19:10:01 +0200

    feat: Show map and player
    Press arrow keys for walking with player.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// Global Game Variables
+const player_start_x = 200;
+const player_start_y = 380;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -24,13 +28,48 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    };
 
+    render() {
+        ctx.drawImage(Resources.get('images/char-boy.png'), this.x, this.y);
+    };
+
+    update() {
+
+    };
+
+    handleInput(key) {
+        console.log(key);
+
+        if (key === 'left' && this.x > 0)
+            this.x = this.x - 100;
+        else if (key === 'right' && this.x < 400)
+            this.x = this.x + 100;
+        else if (key === 'up' && this.y > 60)
+            this.y = this.y - 80;
+        else if (key === 'down' && this.y < 380)
+            this.y = this.y + 80;
+        else
+            console.error('This key cannot be used for playing');
+
+        console.log('x = ' + this.x);
+        console.log('y = ' + this.y);
+    };
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [
+    new Enemy()
+];
 
-
+// Initial player with start position
+let player = new Player(player_start_x, player_start_y);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
